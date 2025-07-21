97259adf70d17ad2e183cbecbea521591bf815d7
Александр Костенков <qazkamaz1@mail.ru>
2025-07-21 22:40:20 +0400

    добавлена модель календаря

--- EasyZap/Data/EasyZapContext.cs
+++ EasyZap/Data/EasyZapContext.cs
@@ -3,9 +3,12 @@ using Microsoft.EntityFrameworkCore;
 
 namespace EasyZap.Data
 {
+    // для миграций
     public class EasyZapContext : DbContext
     {
         public DbSet<Master> Masters { get; set; }
+
+        public DbSet<Appointment> Appointments { get; set; }
 
         public EasyZapContext(DbContextOptions<EasyZapContext> options) : base(options)
         {
---
+++ EasyZap/Models/Appointment.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EasyZap.Models
+{
+    public class Appointment
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public int MasterId { get; set; }
+
+        public Master Master { get; set; }
+
+        [Required]
+        public string ClientName { get; set; }
+
+        [Required]
+        public DateTime DateTime { get; set; }
+
+        [Required]
+        public string Service {  get; set; }
+    }
+}

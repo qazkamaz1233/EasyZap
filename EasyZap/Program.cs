3bdd012743f93092cb8e42fa45b3fb166101c84f
Александр Костенков <qazkamaz1@mail.ru>
2025-08-14 17:16:16 +0400

    Отредактировал лк мастера, добавил фон, убрал лишнее, настроил кнопку выхода

--- EasyZap/Program.cs
+++ EasyZap/Program.cs
@@ -5,7 +5,7 @@ using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
-var builder = WebApplication.CreateBuilder(args);
+var builder = WebApplication.CreateBuilder(args); // builder web приложения
 
 // Add services to the container
 builder.Services.AddControllersWithViews();
@@ -25,7 +25,7 @@ builder.Services.AddAuthorization();
 builder.Services.AddHttpContextAccessor();
 builder.Services.AddScoped<UserService>();
 
-var app = builder.Build();
+var app = builder.Build(); // web приложение
 
 // Middleware pipeline
 if (!app.Environment.IsDevelopment())
@@ -33,13 +33,17 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Home/Error");
 }
 
+// middleware
+
+
 app.UseStaticFiles(); // <- важно для CSS/JS
 app.UseRouting();
 app.UseAuthentication();
 app.UseAuthorization();
+//app.UseCookiePolicy(); // надо проверить
 
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
-app.Run();
+app.Run(); // запуск приложения
